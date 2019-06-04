f5c251b9fa25c7b938d225607e4b55cbac39380a
杨巍 <yang.wei@belle.com.cn>
2019-06-04 14:38:32 +0800

    '修改测试'

--- disconf-web/src/main/java/com/baidu/dsp/common/constant/WebConstants.java
+++ disconf-web/src/main/java/com/baidu/dsp/common/constant/WebConstants.java
@@ -19,6 +19,6 @@ public class WebConstants {
     /**
      * 服务端Controller前缀
      */
-    public static final String API_PREFIX = "/api";
+    public static final String API_PREFIX = "/disconf/api";
 
 }
